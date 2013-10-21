eba7f568cbb21001fc0d3d73803287774268122f
AlexSmerw <neyvniy@mail.ru>
2013-10-21 20:47:03 +0300

    errorhendler+

--- store/views/views.py
+++ store/views/views.py
@@ -1,10 +1,13 @@
 from copy import name
-from flask import Flask, jsonify, render_template, flash, redirect, request
-from app import app
-from forms import LoginForm
+from flask import Flask, jsonify, render_template, flash, redirect, request, make_response
+app = Flask(__name__)
+#from app import app
+#from forms import LoginForm
 from wtforms import form
-
-
+import sys
+sys.path.append('../business_logic')
+from product import list_products, get_product_id, create_product
+from business_logic.validation import ValidationException
 
 @app.route('/login')
 
@@ -24,20 +27,37 @@ def login():
 
     return  render_template('login.html', form=form)
   
-//Products views
-@app.route('/products', methods = ['GET', 'POST'])
+@app.route('/product', methods = ['GET'])
 def products():
+    products_list = list_products()
+    products_arr=[]
+    for i in products_list:
+        products_arr.append({i.id,i.name,i.description,i.description})
 
-    a = request.form["name"]
-    b = request.form["name"]
-    products = [{"id":"1", "name":"apple", "description":"Sweet apple", "price":"10.5", "dimension":"kg"},
-                {"id":"2", "name":a, "description":"Sweet aorange", "price":"15.4", "dimension":"kg"},
-                {"id":"3", "name":"Banana", "description":"Sweet banana", "price":"25.4", "dimension":"kg"}
-                ]
-    return jsonify(products=products)
+    return make_response(jsonify(products=products_arr),200)
+
+@app.route('/product/<id>', methods = ['GET'])
+def products_id():
+    js = request.get_json()
+    product_id = get_product_id(js['id'])
+    product ={product_id.id,product_id.name,product_id.description,product_id.description}
+    resp = make_response(jsonify(products=product),200)
+    return resp
+
+@app.route('/product', methods = ['POST'])
+def products_post():
+    js = request.get_json()
+    create_product(js['name'],js['description'],js['price'],js['id'])
+    resp = make_response(0,200)
+    return resp
+
+@app.errorhandler(ValidationException)
+def err_han(e):
+    error_dict = {'message': e.message}
+    return make_response(jsonify(error_dict), 404)
 
 
-@app.route('/productgrid')
+
+app.route('/productgrid')
 def productgrid():
     return render_template('product_grid.html')
-//end products views
