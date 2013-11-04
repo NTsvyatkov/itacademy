d21c2e049f562de219af460122077b1422d433e7
Sergeybrsa <sergey-friend1986@yandex.ru>
2013-11-05 01:15:28 +0400

    [#:20] 1) Removed test endpoint create_user_data; 	2) Html extension added to the endpoint create_user 	3)Created user.py module into which the endpoint create_user  # Please enter the commit message for your changes. Lines starting

--- store/views/__init__.py
+++ store/views/__init__.py
@@ -1,3 +1,10 @@
+from flask import Flask
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app.config.from_object('config')
+import views.user
+
+
+
 from flask_bootstrap import app
 from views import views_app
 from views import products
---
+++ store/views/user.py
@@ -0,0 +1,8 @@
+
+from views import app
+from flask import Flask, render_template
+
+@app.route('/create_user.html', methods=('GET', 'POST'))
+def create_user():
+
+        return render_template('create_user.html',)
