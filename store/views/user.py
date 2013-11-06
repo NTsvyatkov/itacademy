c52165a286cb86c48b1f4b00e9f96f330df2539c
toha9884 <toha9884@gmail.com>
2013-11-06 17:26:03 +0200

    Add Web Service methods for user entity

--- store/views/user.py
+++ store/views/user.py
@@ -1,8 +1,56 @@
-
 from views import app
-from flask import Flask, render_template
+
+from flask import Flask, jsonify, render_template, request, make_response
+from models.user_dao import UserDao
+from models import db_session
+from flask_bootstrap import app
+from business_logic.user_manager import getListUser, getUserByID, deleteUser, createUser, updateUser
+from business_logic.validation import ValidationException
+
 
 @app.route('/create_user.html', methods=('GET', 'POST'))
 def create_user():
 
         return render_template('create_user.html',)
+        
+@app.route('/user', methods = ['GET'])
+def users():
+    user_list = getListUser()
+    users_arr=[]
+    for i in user_list:
+        users_arr.append({'id':i.id,'login':i.login,'first_name':i.first_name, 'last_name':i.last_name,'email':i.email, 'role_id':i.role_id})
+    return make_response(jsonify(users=users_arr),200)
+
+
+@app.route('/user/<int:id>', methods = ['GET'])
+def users_id(id):
+    i=getUserByID(request.get['id'])
+    user ={'id':i.id,'login':i.login,'first_name':i.first_name, 'last_name':i.last_name,'email':i.email, 'role_id':i.role_id}
+    resp = make_response(jsonify(users=user),200)
+    return resp
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def users_id_delete(id):
+    deleteUser(id)
+    resp = make_response(jsonify({'message':'success'}),200)
+    return resp
+
+
+@app.route('/user', methods = ['POST'])
+def users_post():
+    js = request.get_json()
+    createUser(js['user_id'],js['login'],js['first_name'],js['last_name'],js['password'],js['email'],js['region_id'],js['role_id'])
+    resp = make_response(0,201)
+    return resp
+
+@app.route('/user', methods = ['PUT'])
+def users_update():
+    js = request.get_json()
+    updateUser(js['user_id'],js['login'],js['first_name'],js['last_name'],js['password'],js['email'],js['region_id'],js['role_id'])
+    resp = make_response(0,200)
+    return resp
+
+@app.errorhandler(ValidationException)
+def err_han(e):
+    error_dict = {'message': e.message}
+    return make_response(jsonify(error_dict), 404)
