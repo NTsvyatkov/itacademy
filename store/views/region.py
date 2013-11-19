beff15b63a2756a72df35263b87f2cc22887ba49
Zolotar1988 <denis-zolotar@yandex.ru>
2013-11-19 19:20:15 +0200

    update

--- store/views/region.py
+++ store/views/region.py
@@ -1,7 +1,9 @@
 from flask import jsonify, render_template, request, make_response
 
 from flask_bootstrap import app
-from business_logic.region_manager import getlistRegion, createRegion, updateRegion, deleteRegion, getRegionByID
+from models.region_dao import RegionDao
+from maintenance.pager_region import Pagination
+from business_logic.region_manager import getlistRegion, createRegion, updateRegion, deleteRegion, getRegionByID, getlistRegion
 from business_logic.validation import ValidationException
 
 @app.route('/region', methods=('GET', 'POST'))
@@ -23,6 +25,19 @@ def regions_id(id):
     region ={'region_id': i.region_id, 'name': i.name}
     resp = make_response(jsonify(regions=region), 200)
     return resp
+
+@app.route('/api/regions/<int:page>', methods=['GET'])
+def region_page(page):
+    all_rec = RegionDao.getlistRegion()
+    records_per_page = 5
+    pagination = Pagination(records_per_page, all_rec, page)
+    prods = pagination.pager()
+    records_amount = len(all_rec)
+    regions_arr = []
+    for i in prods:
+        regions_arr.append({'id': i.id, 'name': i.name})
+    return make_response(jsonify(regions=regions_arr, records_amount=records_amount,
+                                 records_per_page=records_per_page), 200)
 
 
 @app.route('/api/regions', methods = ['POST'])
