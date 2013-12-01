4d0d8b9be6359e4882401619156699086ba69a6b
Yelizaveta <plehanovaelizaveta@gmail.com>
2013-12-01 11:31:43 +0200

    Update product_dimension_manager.py

--- store/business_logic/product_dimension_manager.py
+++ store/business_logic/product_dimension_manager.py
@@ -15,34 +15,33 @@ def validate_dimension_id(id):
         raise NotFoundException("Unable to find dimension with given id")
 
 def list_dimension():
-    d_list = Product.get_all_dimensions()
-    return p_list
+    d_list = Dimension.get_all_dimensions()
+    return d_list
 
 
 def get_dimension_by_id(id):
     validate_dimension_id(id)
-    dimension_by_id = dimension.get_dimension(id)
+    dimension_by_id = Dimension.get_dimension(id)
     return dimension_by_id
 
 
 def create_dimension(name, id):
     validate_name(name)
     validate_dimension_id(id)
-    dimension.add_dimension(name, id)
+    Dimension.add_dimension(name, id)
 
 
 def update_dimension(id, new_name):
     validate_dimension_id(id)
     validate_name(new_name)
-    validate_price(new_price)
-    dimension.upd_dimension(id, new_name)
+    Dimension.upd_dimension(id, new_name)
 
 
 def delete_dimension(id):
     validate_dimension_id(id)
-    dimension.del_dimension(id)
+    Dimension.del_dimension(id)
 
 
 def list_dimensions():
-        d_list = dimension.get_all_dimensions()
+        d_list = Dimension.get_all_dimensions()
         return d_list
