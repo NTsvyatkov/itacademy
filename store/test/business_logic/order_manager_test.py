287cc90b54150f225bbcebf3077ed5768a0c1679
andrey-svistovich <andrey.svistovich@gmail.com>
2013-12-08 18:38:49 +0200

    [#70] Unit tests coverage

--- store/test/business_logic/order_manager_test.py
+++ store/test/business_logic/order_manager_test.py
@@ -1,6 +1,6 @@
 import unittest
-from mock import Mock, MagicMock
-from business_logic.order_manager import addProductToCartStatus, addOrderWithStatusCart
+from mock import patch
+from business_logic import order_manager
 from models.order_dao import Order, OrderProduct
 
 
@@ -12,19 +12,31 @@ class OrderTest(unittest.TestCase):
         self.productId = 11
         self.dimension_id = 2
 
-    def testAddOrderWithStatusCart(self):
-        mock = Mock()
-        mock.testMethod( Order.getOrderByStatus(self.userId).id , key="4" )
-        mock.mockCheckCall(self, 0, 'testMethod', "hello", key="some value" )
-        #real.method(key='4')
-        #order = Order.getOrderByStatus(self.userId)
-        #self.assertEqual(self.userId,order.user_id)
+    @patch('business_logic.order_manager.Order.getAllOrders')
+    def testGetListOrders(self, getAllOrders):
+        order_manager.getListOrder()
+        self.assertEqual(getAllOrders.call_count, 1)
 
-    def testAddProductToCartStatus(self):
-        addProductToCartStatus(self.userId, self.productId, self.json)
-        order = Order.getOrderByStatus(self.userId)
-        product = OrderProduct.get_order_product(order.id,self.productId, self.dimension_id)
-        self.assertEqual(self.productId, product.product_id)
+    @patch('business_logic.order_manager.OrderStatus.get_all_order_status')
+    def testStatusList(self, getListStatus):
+        order_manager.list_status()
+        self.assertEqual(getListStatus.call_count, 1)
+
+    @patch('business_logic.order_manager.DeliveryType.get_delivery_all')
+    def testDeliveryList(self, getDeliveryList):
+        order_manager.list_delivery()
+        self.assertEqual(getDeliveryList.call_count, 1)
+
+    @patch('business_logic.order_manager.Order.getOrderByStatus')
+    @patch('business_logic.order_manager.OrderProduct.get_order_product')
+    @patch('business_logic.order_manager.OrderProduct.updateSumQuantity')
+    @patch('business_logic.order_manager.OrderProduct.add_order_product')
+    def testAddProductToCartStatus(self, orderStatus, orderProduct, updateSum, addOrder):
+        order_manager.addProductToCartStatus(self.userId,self.productId, self.json)
+        self.assertEqual(orderStatus.call_count, 0)
+        self.assertEqual(orderProduct.call_count, 1)
+        self.assertEqual(updateSum.call_count, 1)
+        self.assertEqual(addOrder.call_count, 1)
 
 if __name__ == '__main__':
     unittest.main()
--- store/test/business_logic/user_manager_test.py
+++ store/test/business_logic/user_manager_test.py
@@ -1,6 +1,7 @@
 import unittest
+from business_logic import user_manager
 from business_logic.user_manager import *
-
+from mock import patch
 
 class UserManagerTest(unittest.TestCase):
 
@@ -11,7 +12,9 @@ class UserManagerTest(unittest.TestCase):
         self.password = 'password'
         self.email = 'alex.ivanov@gmail.ru'
         self.roleId = 1
+        self.regionId = 1
         self.userId = 1
+
 
     def testCorrectLogin(self):
         self.assertRaises(ValidationException,validationLogin(self.login))
@@ -33,6 +36,28 @@ class UserManagerTest(unittest.TestCase):
 
     def testCorrectEmail(self):
         self.assertRaises(ValidationException, validationEmail(self.email))
+
+    @patch('business_logic.user_manager.UserDao.getAllUsers')
+    def testGetListUser(self,getAllUsers):
+        user_manager.getListUser()
+        self.assertEqual(getAllUsers.call_count, 1)
+
+    @patch('business_logic.user_manager.UserDao.updateUser')
+    def testUpdateUser(self, updateUser):
+        user_manager.updateUser(self.userId,self.login, self.firstName, self.lastName, self.password, self.email,
+                                self.roleId, self.regionId)
+        self.assertEqual(updateUser.call_count, 1)
+
+    @patch('business_logic.user_manager.UserDao.deleteRecord')
+    def testDeleteUser(self,deleteUser):
+        user_manager.deleteUser(self.userId)
+        self.assertEqual(deleteUser.call_count, 1)
+
+    @patch('business_logic.user_manager.UserDao.getUserByID')
+    def testGetUser(self, getUser):
+        user_manager.getUserByID(self.userId)
+        self.assertEqual(getUser.call_count, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
